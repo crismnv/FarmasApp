22553ddc4b43f8405b8eadb3bcfa6961e811b3ab
crismnv <crisycochea@gmail.com>
2017-11-19 20:07:51 -0500

    Base de datos - Errores

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -34,15 +34,26 @@
             <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+            {{-- <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
             <li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
                     <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
                 </ul>
+            </li> --}}
+            <li class="treeview">
+                <a href="#"><i class='sidebar-seguridad fa fa-unlock'></i> <span>Seguridad</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('admin/users')}}">Usuarios</a></li>
+                    <li><a href="{{url('admin/roles')}}">Roles</a></li>
+                    <li><a href="{{url('admin/permissions')}}">Permisos</a></li>
+                    {{-- <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li> --}}
+                </ul>
             </li>
+
         </ul><!-- /.sidebar-menu -->
+
     </section>
     <!-- /.sidebar -->
 </aside>
